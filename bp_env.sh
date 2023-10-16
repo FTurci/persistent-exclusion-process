f9cffe4636a4d1345e40ba5ed78343de9af4e9ba
lckdscl <lckdscl@bim.boats>
2023-10-16 15:21:12 +0100

    feat: update dependencies

--- bp_env.sh
+++ bp_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for pkg in h5py tqdm matplotlib numpy keras tensorflow
+for pkg in h5py tqdm matplotlib numpy keras protobuf==3.6.1 tensorflow
 do
     ins=$(pip3 list --format legacy | grep -c "$pkg")
     if [ "$ins" -eq 1 ]
